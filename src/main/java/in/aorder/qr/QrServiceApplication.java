65604c228ed98b9024adafb276b41c7123e841c4
Soumya Deb <soumyadeb1809@gmail.com>
2021-06-15 19:51:06 +0530

    Initial commit

---
+++ src/main/java/in/aorder/qr/QrServiceApplication.java
@@ -0,0 +1,13 @@
+package in.aorder.qr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QrServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QrServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/in/aorder/qr/QrServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package in.aorder.qr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QrServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
